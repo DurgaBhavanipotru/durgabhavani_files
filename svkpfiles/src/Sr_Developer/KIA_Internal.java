914d282e5470321b3a41f771216a4961df7241b2
LPT-27 <LPT-27@192.168.1.72>
2024-11-19 16:36:50 +0530

    12TH COMMIT

---
+++ svkpfiles/src/Sr_Developer/KIA_Internal.java
@@ -0,0 +1,12 @@
+package Sr_Developer;
+import Jr_Developer.*;
+
+public class KIA_Internal {
+
+	public static void main(String[] args) {
+		KIA_AirBags obj=new KIA_AirBags();
+		obj.disp_AirBags();
+    KIA_Speed_limit obj1=new  KIA_Speed_limit();
+    obj1.disp_Speed_limit();
+	}
+}
